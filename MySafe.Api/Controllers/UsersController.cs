74d8e06f61de29073f21c57786f7b6ec0a941bcc
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-01-29 21:19:37 +1000

    Добавьте файлы проекта.

---
+++ MySafe.Api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySafe.Api.Models;
+
+namespace MySafe.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly MySafeDbContext _context;
+
+        public UsersController(MySafeDbContext context)
+        {
+            _context = context;
+        } 
+    }
+}
